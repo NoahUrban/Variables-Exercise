873dfa9b0fea05909d4f47ef7998eed156c6d47f
“Noah <urbannoah@yahoo.com>
2024-05-31 11:02:43 -0400

    add and display variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Variable Declarations
+            string game = "Overwatch";
+            int year = 2016;
+            char rating = 'B';
+            double percent = 81.19;
+            decimal decrease = 6.295m;
+            bool isFun = true;
+
+            // Output 
+            Console.WriteLine($"{game} was released in {year} and currently has a game rating of {rating} at an {percent}% approval score.\n");
+            Console.WriteLine($"The game's active playerbase has decreased to {decrease} million players, but is it still fun?\n");
+            Console.WriteLine($"Still fun = {isFun}");
         }
     }
 }
